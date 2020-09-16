16e4fc58141404a767832d923f61eb6685475881
DmitryMI <wet75787@tqosi.com>
2020-09-16 19:24:52 +0300

    PC RESET COMMIT

---
+++ Lab08/AdoNetLab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new[] {1, 2, 3};
+            PrintCollection(values);
+            List<double> list = new List<double>() {1, 2, 3};
+            PrintCollection(list);
+
+            Console.ReadKey();
+        }
+
+        static void PrintCollection(ICollection collection)
+        {
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element.ToString());
+            }
+        }
+    }
+}
